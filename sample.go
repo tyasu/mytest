5fbb01acf34a0d5ecd85b65382b78c421f5a5a8e
tyasuno <ink890ctok@gmail.com>
2016-10-05 17:20:15 +0900

    rename function

--- sample.go
+++ sample.go
@@ -21,13 +21,13 @@ type issueInfo struct {
   createdAt string `json:"createdAt"`
 }
 
-func (t *myChaincode) Run(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
-    fmt.Println("Run is running " + function +", with args",len(args))
+func (t *myChaincode) Invoke(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
+    fmt.Println("Invoke is running " + function +", with args",len(args))
 
 	if function == "init" {
 		return t.Init(stub, "init", args)
-	} else if function == "issue" {
-		return t.issue(stub, args)
+	} else if function == "write" {
+		return t.write(stub, args)
 	}
 	return nil, errors.New("Received unknown invoke function name")
 }
@@ -37,6 +37,22 @@ func (t *myChaincode) Run(stub *shim.ChaincodeStub, function string, args []stri
 
 // Init takes a string and int. These are stored as a key/value pair in the state
 func (t *myChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+
+	if len(args) != 1 {
+			 return nil, errors.New("Incorrect number of arguments. Expecting 1")
+	 }
+
+	 err := stub.PutState("hello_world", []byte(args[0]))
+	 if err != nil {
+			 return nil, err
+	 }
+
+	 return nil, nil
+
+}
+
+// Invoke is a no-op
+func (t *myChaincode) Write(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	var info issueInfo
 	var ammount int
 	var data []string
@@ -61,19 +77,6 @@ func (t *myChaincode) Init(stub shim.ChaincodeStubInterface, function string, ar
 	if err != nil {
 		return nil, errors.New("Error putting data on ledger")
 }
-
-var empty []string
-	jsonAsBytes, _ := json.Marshal(empty)								//marshal an emtpy array of strings to clear the index
-	err = stub.PutState(moneyIndexStr, jsonAsBytes)
-	if err != nil {
-		return nil, err
-}
-
-	return nil, nil
-}
-
-// Invoke is a no-op
-func (t *myChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	return nil, nil
 }
 
@@ -88,6 +91,24 @@ func (t *myChaincode) Query(stub *shim.ChaincodeStub, function string, args []st
 	fmt.Println("query did not find func: " + function)						//error
 
 	return nil, errors.New("Received unknown function query")
+}
+
+func (t *myChaincode) read(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
+    var key, jsonResp string
+    var err error
+
+    if len(args) != 1 {
+        return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
+    }
+
+    key = args[0]
+    valAsbytes, err := stub.GetState(key)
+    if err != nil {
+        jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
+        return nil, errors.New(jsonResp)
+    }
+
+    return valAsbytes, nil
 }
 
 
