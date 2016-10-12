7d9c819c0ae6f3764b0174f6d54fa6e027c6e328
tyasuno <ink890ctok@gmail.com>
2016-10-12 16:22:46 +0900

    modify init and read function

--- sample.go
+++ sample.go
@@ -29,26 +29,17 @@ func main() {
 	}
 }
 
-// Init takes a string and int. These are stored as a key/value pair in the state
 func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
 
-var info Info
+fmt.Println("Initializing data")
+var blank []string
+	blankBytes, _ := json.Marshal(&blank)
+	err := stub.PutState("data", blankBytes)
+    if err != nil {
+        fmt.Println("Failed to initialize")
+    }
 
-info.thing="apple"
-info.madeBy="me"
-info.amount="1"
-info.createdAt="2016"
-
-	if len(args) != 1 {
-			 return nil, errors.New("Incorrect number of arguments. Expecting 1")
-	 }
-
-	 jsonAsBytes, _ := json.Marshal(info)
-	 err := stub.PutState("data", jsonAsBytes)
-	 if err != nil {
-			 return nil, err
-	 }
-
+	fmt.Println("Initialization complete")
 	 return nil, nil
 
 }
@@ -108,6 +99,7 @@ func (t *SimpleChaincode) Query(stub *shim.ChaincodeStub, function string, args 
 func (t *SimpleChaincode) read(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
     var key, jsonResp string
     var err error
+		var info Info
 
     if len(args) != 1 {
         return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
@@ -115,11 +107,11 @@ func (t *SimpleChaincode) read(stub *shim.ChaincodeStub, args []string) ([]byte,
 
     key = args[0]
     valAsbytes, err := stub.GetState(key)
-
+		err = json.Unmarshal(valAsbytes, &info)
     if err != nil {
         jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
         return nil, errors.New(jsonResp)
     }
 
-    return valAsbytes, nil
+    return info, nil
 }
