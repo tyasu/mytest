b03db456faa3f830f87e53b3e7284c707b0b6e50
tyasuno <ink890ctok@gmail.com>
2016-10-12 17:03:12 +0900

    modify returnned value in error handling logic in read().

--- sample.go
+++ sample.go
@@ -113,16 +113,17 @@ func (t *SimpleChaincode) read(stub *shim.ChaincodeStub, args []string) (Info, e
     var err error
 		var info Info
 
-    if len(args) != 1 {
-        return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
-    }
-
     key = args[0]
     valAsbytes, err := stub.GetState(key)
+    if err != nil {
+		fmt.Println("Error retrieving info " + key)
+		return cp, errors.New("Error retrieving info " + key)
+	}
+	
 		err = json.Unmarshal(valAsbytes, &info)
     if err != nil {
         jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
-        return nil, errors.New(jsonResp)
+        return info, errors.New(jsonResp)
     }
 
     return info, nil
